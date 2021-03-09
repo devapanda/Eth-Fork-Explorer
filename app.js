be1a37c7f0f519292525be9b558212f5fc1a73b1
root <root@vmi432781.contaboserver.net>
2021-03-09 20:01:27 +0100

    commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ try {
 }
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
